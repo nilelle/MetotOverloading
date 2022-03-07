9d300876e60760127cebe8c978709c85631b1576
Bora Kaya <bora.587@hotmail.com>
2022-03-07 21:10:01 +0300

    Add project files.

---
+++ MetotOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOverloading
+{
+    
+    internal class Program
+    {
+        public static int Toplam(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+            return toplam;
+        }
+        static void Main(string[] args)
+        {
+            Toplam(4, 8, out int toplam);
+            Console.WriteLine(toplam);
+
+            
+        }
+    }
+}
